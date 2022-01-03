8f7428573744356ac3a0fb4b3bdf23698347b681
Amrit Pal Singh <amrit.s@proximity.tech>
2022-01-04 01:36:59 +0530

    user create using post route

--- routes/user.js
+++ routes/user.js
@@ -15,8 +15,44 @@ router.get('/', async (req, res) => {
         //     username: "amr2"
         // }
     });
-    console.log(users);
+    //console.log(users);
     res.json(users);
+})
+
+//create user
+router.post('/createUser', async(req, res) => {
+    //console.log("req received here => ", req);
+    const { username } = req.body;
+    console.log("username received here => ", username);
+
+    const userExists = await user.findUnique({
+        where : {
+            username : username
+        },
+        select : {
+            username : true
+        }
+    })
+    // .catch((err) => {
+    //     res.json({"err" : err});
+    // });
+    //res.json(userExists);
+
+    if(userExists){
+        return res.status(400).json({
+            msg : "User already exists!!"
+        })
+    }
+
+    console.log("create new user here..!!");
+    const newUser = await user.create({
+        data : {
+            username
+        }
+    });
+
+    res.json(newUser);
+
 })
 
 module.exports = router;
